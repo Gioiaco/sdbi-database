7d2f8612160dc5351b2521a1971d1f265e3265c9
Gioiaco <iacobellisgiovanni83@gmail.com>
2025-03-29 12:58:30 +0100

    Add files via upload

---
+++ query1.sql
@@ -0,0 +1,10 @@
+SELECT COUNT(b.ID_Biglietto) AS Numero_BigliettiVenduti
+FROM Biglietti b
+JOIN Corse c ON b.Corsa_ID = c.ID_Corsa
+JOIN Tratte t ON c.Tratta_ID = t.ID_Tratta
+WHERE t.Stazione_Partenza_ID = (SELECT ID_Stazione FROM Stazioni WHERE Nome_Stazione = 'Firenze SMN')
+  AND t.Stazione_Arrivo_ID = (SELECT ID_Stazione FROM Stazioni WHERE Nome_Stazione = 'Milano Centrale')
+  AND c.Data_Ora_Partenza BETWEEN '2025-04-01' AND '2025-04-30';
+
+
+
